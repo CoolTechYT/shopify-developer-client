ed2c4d8916a62ace2bbd1e12eb84a6fc8340b1b3
XanMan336 <4UXX2wMa9BuNJ5KBiKW8@tutanota.com>
2022-06-15 17:52:43 -0400

    Add TikTok images

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -60,8 +60,9 @@ const Hero = ({
       <div className="container-sm">
         <div className={innerClasses}>
           <div className="hero-content">
+            <br />
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Drive more traffic, <span className="text-color-primary">turbo</span>
             </h1>
             {/* <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
